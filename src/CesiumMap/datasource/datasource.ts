694e320990e70fc542576d9dec3b5a85d7641851
张世烨 <zhangshiye19@outlook.com>
2024-01-17 23:26:47 +0800

    ds进行数据添加

---
+++ src/CesiumMap/datasource/datasource.ts
@@ -0,0 +1,36 @@
+import * as Cesium from 'cesium';
+import CesiumMap from "@/CesiumMap/CesiumMap";
+
+class CesiumDS {
+    static instance: CesiumDS;
+    dss: Record<string, Cesium.DataSource>;
+    viewer: Cesium.Viewer;
+
+    constructor(viewer: Cesium.Viewer) {
+        this.dss = {}
+        this.viewer = viewer;
+    }
+
+    getDSById(name: string) {
+        if (!this.dss.hasOwnProperty(name)) {
+            this.dss[name] = new Cesium.DataSource()
+            this.viewer.dataSources.add(this.dss[name]) //需要吗？
+        }
+        return this.dss[name];
+    }
+
+    hasDS(name: string) {
+        return this.dss.hasOwnProperty(name)
+    }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new CesiumDS(CesiumMap.getViewer())
+        }
+        return this.instance
+    }
+
+}
+
+export default CesiumDS.getInstance()
+
